d6cc28dcc9e976826eee9b03c2f15e15c3679e7c
Kirill Demidov <kademidov@gmail.com>
2020-02-27 12:57:20 +0300

    add gulp-file-include plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,49 @@
 'use strict';
 
-const { series, parallel, src, dest, watch } = require('gulp');
-const less = require('gulp-less');
-const postcss = require('gulp-postcss');
-const mqpacker = require('css-mqpacker');
-const sourcemaps = require('gulp-sourcemaps');
-const notify = require('gulp-notify');
-const browserSync = require('browser-sync').create();
+const { series, parallel, src, dest, watch }    = require('gulp');
+const fileinclude                               = require('gulp-file-include');
+const less                                      = require('gulp-less');
+const postcss                                   = require('gulp-postcss');
+const mqpacker                                  = require('css-mqpacker');
+const sourcemaps                                = require('gulp-sourcemaps');
+const notify                                    = require('gulp-notify');
+const browserSync                               = require('browser-sync').create();
 
 // Path
 const path = {
     www: {
         style: 'www/css/',
-        html : 'www/*.html'
+        html : 'www/'
     },
     src: {
-        style: 'src/css/*.less'
+        style: 'src/css/*.less',
+        html: 'src/html/*.html'
     },
     watch: {
         srcStyle   : 'src/css/**/*.less',
         buildStyle : 'www/css/*.css',
+        srcHtml    : 'src/html/**/*.html',
         html       : 'www/*.html'
     }
 }
+
+// concat html
+function html() {
+    return src(path.src.html)
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        })
+            .on('error', notify.onError({
+                message: '<%= error.fileName %>' +
+                    '\nLine <%= error.lineNumber %>:' +
+                    '\n<%= error.message %>',
+                title  : '<%= error.plugin %>'
+            }))
+        )
+        .pipe(dest(path.www.html));
+}
+exports.html = html;
 
 // Compilation less
 function styles() {
@@ -56,12 +77,16 @@ function serve() {
     ], styles);
 
     watch([
+        path.watch.srcHtml
+    ], html);
+
+    watch([
         path.watch.html,
         path.watch.buildStyle
     ]).on('change', browserSync.reload);
 }
 
 exports.default = series(
-    parallel(styles),
+    parallel(styles, html),
     serve
 );
